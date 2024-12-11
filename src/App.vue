f43996622a69e868ef723402ec2bfaf2a35691f5
Ullus <nikita96piter@gmail.com>
2024-12-12 02:30:14 +0300

    Add color variables to Vuetify theme

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <template>
 	<v-app>
-		<v-navigation-drawer width="auto" location="right" class="bg-secondary">
-			<v-list>
-				<v-list-item>
-					<v-btn variant="text" icon="mdi-wifi" color="primary"> </v-btn>
-				</v-list-item>
-				<v-list-item>
-					<v-btn variant="text" icon="mdi-wifi" color="primary"> </v-btn>
-				</v-list-item>
-				<v-list-item>
-					<v-btn variant="text" icon="mdi-wifi" color="primary"> </v-btn>
-				</v-list-item>
-
-				<v-divider thickness="2" length="80%"></v-divider>
-
-				<v-list-item>
-					<v-btn variant="text" icon="mdi-wifi" color="color-text-700"> </v-btn>
-				</v-list-item>
-				<v-list-item>
-					<v-btn variant="text" icon="mdi-wifi" color="primary"> </v-btn>
-				</v-list-item>
-			</v-list>
-		</v-navigation-drawer>
-
+		<AsideMenu />
 		<v-main>
 			<router-view />
 			<v-btn>Текст</v-btn>
@@ -32,10 +10,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import AsideMenu from './components/AsideMenu.vue';
 
 export default defineComponent({
-	name: 'App',
-
+	components: { AsideMenu },
 	data() {
 		return {
 			//
---
+++ src/components/AsideMenu.vue
@@ -0,0 +1,36 @@
+<template>
+	<v-navigation-drawer width="auto" location="right" class="bg-secondary">
+		<v-list>
+			<v-list-item>
+				<v-btn variant="text" icon="mdi-wifi" color="primary"> </v-btn>
+			</v-list-item>
+			<v-list-item>
+				<v-btn variant="text" icon="mdi-wifi" color="primary"> </v-btn>
+			</v-list-item>
+			<v-list-item>
+				<v-btn variant="text" icon="mdi-wifi" color="primary"> </v-btn>
+			</v-list-item>
+
+			<v-divider thickness="2" length="80%"></v-divider>
+
+			<v-list-item>
+				<v-btn variant="text" icon="mdi-wifi" color="color-bg-050"> </v-btn>
+			</v-list-item>
+			<v-list-item>
+				<v-btn variant="text" icon="mdi-wifi" color="primary"> </v-btn>
+			</v-list-item>
+		</v-list>
+	</v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	setup() {
+		return {};
+	},
+});
+</script>
+
+<style scoped></style>
