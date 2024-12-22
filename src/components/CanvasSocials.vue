674ecfd89b88e55b5e13f3826228b91739c40eb4
Ullus <nikita96piter@gmail.com>
2024-12-23 02:33:35 +0300

    Change the template of Socials block component

--- src/components/CanvasSocials.vue
+++ src/components/CanvasSocials.vue
@@ -5,9 +5,34 @@
 			<p class="info__address">с.Одесское, ул.Ленина, д.27</p>
 		</div>
 
-		<div class="info__socials"></div>
+		<div class="info__socials">
+			<div class="info__social-item">
+				<IconBase :width="18" :height="18">
+					<IconOk />
+				</IconBase>
+				<p class="info__social-text">ok.ru/odesskyrkd</p>
+			</div>
+			<div class="info__social-item">
+				<IconBase :width="18" :height="18">
+					<IconVk />
+				</IconBase>
+				<p class="info__social-text">vk.com/odess_kino</p>
+			</div>
+			<div class="info__social-item info__social-item--single">
+				<IconBase :width="18" :height="18">
+					<IconLink />
+				</IconBase>
+				<p class="info__social-text">odesskoekdc.omsk.muzkult.ru</p>
+			</div>
+		</div>
 
-		<p>&nbsp;- возможность оплаты по Пушкинской Карте</p>
+		<div class="info__pCard-wrapper">
+			<PushkinCardLabel :p-card="true" />
+			<span>-</span>
+			<span class="info__pCard-desc"
+				>возможность оплаты по Пушкинской Карте</span
+			>
+		</div>
 	</div>
 </template>
 
@@ -17,9 +42,10 @@ import IconBase from '@/components/ui/icon/IconBase.vue';
 import IconOk from '@/components/ui/icon/icons/IconOk.vue';
 import IconVk from '@/components/ui/icon/icons/IconVk.vue';
 import IconLink from '@/components/ui/icon/icons/IconLink.vue';
+import PushkinCardLabel from '@/components/ui/PushkinCardLabel.vue';
 
 export default defineComponent({
-	components: { IconBase, IconOk, IconVk, IconLink },
+	components: { IconBase, IconOk, IconVk, IconLink, PushkinCardLabel },
 	setup() {
 		return {};
 	},
