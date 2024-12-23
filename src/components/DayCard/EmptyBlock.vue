349e2f34813fc31489008458b1a329f0e8d08d62
Ullus <nikita96piter@gmail.com>
2024-12-23 03:17:50 +0300

    Add animations to an Empty element, add delete button

--- src/components/DayCard/EmptyBlock.vue
+++ src/components/DayCard/EmptyBlock.vue
@@ -4,28 +4,50 @@
 		:class="{ editable: $store.state.isEditing }"
 		:style="{ height: height + '%' }"
 	>
-		<p class="empty__desc" :class="{ hide: height === 10 }">Высота</p>
-		<div class="empty__btns-wrapper">
-			<v-btn density="compact" variant="text" @click="setHeight(10)">10%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(20)">20%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(30)">30%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(40)">40%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(50)">50%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(60)">60%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(70)">70%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(80)">80%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(90)">90%</v-btn>
-			<v-btn density="compact" variant="text" @click="setHeight(100)"
-				>100%</v-btn
+		<template v-if="$store.state.isEditing">
+			<p class="empty__desc" :class="{ compact: height === 10 }">Высота</p>
+			<div class="empty__btns-wrapper">
+				<v-btn density="compact" variant="text" @click="setHeight(10)"
+					>10%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(20)"
+					>20%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(30)"
+					>30%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(40)"
+					>40%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(50)"
+					>50%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(60)"
+					>60%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(70)"
+					>70%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(80)"
+					>80%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(90)"
+					>90%</v-btn
+				>
+				<v-btn density="compact" variant="text" @click="setHeight(100)"
+					>100%</v-btn
+				>
+			</div>
+			<v-btn
+				class="empty__btn"
+				:class="{ compact: height === 10 }"
+				block
+				variant="text"
+				prepend-icon="mdi-trash-can"
+				color="color-danger-200"
+				>Удалить</v-btn
 			>
-		</div>
-		<v-btn
-			class="empty__btn"
-			block
-			prepend-icon="mdi-trash-can"
-			color="color-danger-200"
-			>Удалить</v-btn
-		>
+		</template>
 	</div>
 </template>
 
